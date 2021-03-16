2f47b159c4338eb196e7610eacb800733e334b0a
Jagatperumal <74633014+Jagatperumal@users.noreply.github.com>
2021-03-16 18:54:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+ var canvas =
+document.getElementById("myCanvas");
+ctx =canvas.getcontext("2d");
+ctx.beginPath();
+ctx.strokeStyle- red;
+ctx.lineWidth- 1;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle- blue;
+ctx.lineWidth- 1;
+ctx.arc(250,210,40,0,2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle- yellow;
+ctx.lineWidth- 1;
+ctx.arc(250,210,40,0,2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle- black;
+ctx.lineWidth- 1;
+ctx.arc(250,210,40,0,2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle- green;
+ctx.lineWidth- 1;
+ctx.arc(250,210,40,0,2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle- red;
+ctx.lineWidth- 1;
+ctx.arc(250,210,40,0,2);
+ctx.stroke();
